fa666dc890b35bb5071531acc5278f6077873f90
Daniel Dela Justina <daniel.dela.justina@accenture.com>
2022-08-01 17:31:25 -0300

    add config jest in project, and remove jasmine

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ToastrModule } from 'ngx-toastr';
 
 import { LoginComponent } from './login.component';
 
@@ -8,6 +9,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        ToastrModule.forRoot(),
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
